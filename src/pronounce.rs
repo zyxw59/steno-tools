973d538c40dc5aca03884733b63c94f1e17d80b5
emily Crandall Fleischman <zyxw59@gmail.com>
2024-03-05 22:35:06 -0500

    regex-based syllabizer

--- src/pronounce.rs
+++ src/pronounce.rs
@@ -1,9 +1,11 @@
 use std::{
+    cmp,
     collections::{BTreeMap, BTreeSet},
     fmt,
     rc::Rc,
 };
 
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 
 use crate::{
@@ -79,92 +81,194 @@ impl<'de> Deserialize<'de> for Pronunciation {
     }
 }
 
-#[derive(Clone, Hash, Eq, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
+#[derive(Clone, Hash, Eq, PartialEq, Deserialize, Serialize)]
 #[serde(transparent)]
 pub struct Phoneme(Rc<str>);
 crate::fmt_impls!(Phoneme);
 crate::deref_impls!(Phoneme as str);
 
-#[derive(Debug, Clone, Deserialize)]
-pub struct Phonology<P: Ord> {
-    onset_singles: BTreeSet<P>,
-    onset_clusters: BTreeMap<P, BTreeSet<P>>,
-    vowels: BTreeSet<P>,
+/// Order phonemes first by decreasing length order, then by normal string ordering. This is so
+/// that when constructing regexes, the sorted order will produce a regex that preferentially
+/// matches longer phonemes.
+impl Ord for Phoneme {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        self.len()
+            .cmp(&other.len())
+            .reverse()
+            .then_with(|| self.0.cmp(&other.0))
+    }
 }
 
-impl<P: Ord> Phonology<P> {
+impl PartialOrd for Phoneme {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct RawPhonology {
+    onset_singles: BTreeSet<Phoneme>,
+    onset_clusters: Vec<OnsetCluster>,
+    vowels: BTreeSet<Phoneme>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct OnsetCluster {
+    first: BTreeSet<Phoneme>,
+    second: BTreeSet<Phoneme>,
+}
+
+impl TryFrom<RawPhonology> for Phonology {
+    type Error = regex::Error;
+
+    fn try_from(raw: RawPhonology) -> Result<Self, Self::Error> {
+        fn pattern_from_set(
+            set: BTreeSet<Phoneme>,
+            right_anchored: bool,
+        ) -> Result<Regex, regex::Error> {
+            let initial = if right_anchored {
+                "(?:".to_owned()
+            } else {
+                String::new()
+            };
+            let mut pattern = set.into_iter().fold(initial, |mut pat, phon| {
+                regex_syntax::escape_into(&phon, &mut pat);
+                pat.push('|');
+                pat
+            });
+            // remove trailing '|'
+            pattern.pop();
+            if right_anchored {
+                pattern += ")$";
+            }
+            Regex::new(&pattern)
+        }
+
+        let onset_singles = pattern_from_set(raw.onset_singles, true)?;
+
+        // map (second_consonant -> [first_consonants])
+        let mut onset_clusters_map = BTreeMap::<_, BTreeSet<_>>::new();
+        for cluster in raw.onset_clusters {
+            for ph in cluster.second {
+                onset_clusters_map
+                    .entry(ph)
+                    .or_default()
+                    .extend(cluster.first.iter().cloned());
+            }
+        }
+        let onset_clusters = onset_clusters_map
+            .into_iter()
+            .map(|(first, seconds)| pattern_from_set(seconds, true).map(|pat| (first, pat)))
+            .collect::<Result<_, _>>()?;
+
+        let vowels = pattern_from_set(raw.vowels, false)?;
+
+        Ok(Phonology {
+            onset_singles,
+            onset_clusters,
+            vowels,
+        })
+    }
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(try_from = "RawPhonology")]
+pub struct Phonology {
+    onset_singles: Regex,
+    onset_clusters: BTreeMap<Phoneme, Regex>,
+    vowels: Regex,
+}
+
+impl Phonology {
     #[allow(unused)]
     pub fn syllabize_word<'p, 'w>(
         &'p self,
-        word: &'w [P],
-    ) -> anyhow::Result<SyllableIterator<'p, 'w, P>> {
-        let (mut onset_start, mut coda_start) = self.syllabize_first(word, 0);
-        if onset_start != 0 {
+        word: &'w str,
+    ) -> anyhow::Result<SyllableIterator<'p, 'w>> {
+        let initial = self.syllabize_one(word, 0);
+        if initial.onset != 0 {
             return Err(anyhow::anyhow!(
-                "failed to syllabize first {onset_start} phonemes"
+                "failed to syllabize first {} phonemes",
+                initial.onset,
             ));
         }
         Ok(SyllableIterator {
-            onset_start,
-            coda_start,
+            prev_syllable: initial,
             word,
             phonology: self,
         })
     }
 
-    /// Returns the index of the start of the next syllable, and the index after the vowel in that
-    /// syllable.
-    fn syllabize_first(&self, word: &[P], start_at: usize) -> (usize, usize) {
+    fn syllabize_one(&self, word: &str, start_at: usize) -> SyllableIndices {
         // find the first vowel
-        let Some(vowel_idx) = word[start_at..]
-            .iter()
-            .position(|ph| self.vowels.contains(ph))
-        else {
-            return (word.len(), word.len());
+        let Some(vowel_match) = self.vowels.find_at(word, start_at) else {
+            return SyllableIndices {
+                onset: word.len(),
+                vowel: word.len(),
+                coda: word.len(),
+            };
         };
-        let vowel_idx = vowel_idx + start_at;
+        let vowel = vowel_match.start();
+        let coda = vowel_match.end();
         // work backwards to find the maximally valid onset
-        let mut onset = word[..vowel_idx].iter().enumerate().rev();
-        let last_item = match onset.next() {
-            Some((idx, ph)) if self.onset_singles.contains(ph) => (idx, ph),
-            _ => return (vowel_idx, vowel_idx + 1),
-        };
-        let result = onset.try_fold(last_item, |(following_idx, following_ph), (idx, ph)| {
-            if self
-                .onset_clusters
-                .get(ph)
-                .map_or(false, |followers| followers.contains(following_ph))
-            {
-                Ok((idx, ph))
-            } else {
-                Err(following_idx)
-            }
-        });
-        match result {
-            Ok((idx, _)) | Err(idx) => (idx, vowel_idx + 1),
+        let mut onset = vowel;
+        let mut following_maybe = self.onset_singles.find_at(&word[..onset], start_at);
+        while let Some(following) = following_maybe {
+            onset = following.start();
+            let Some(prev_regex) = self.onset_clusters.get(following.as_str()) else {
+                break;
+            };
+            following_maybe = prev_regex.find_at(&word[..onset], start_at);
         }
+        SyllableIndices { onset, vowel, coda }
     }
 }
 
-pub struct SyllableIterator<'p, 'w, P: Ord> {
-    phonology: &'p Phonology<P>,
-    word: &'w [P],
-    onset_start: usize,
-    coda_start: usize,
+/// Start indices of the onset, vowel, and coda of a syllable
+struct SyllableIndices {
+    onset: usize,
+    vowel: usize,
+    coda: usize,
 }
 
-impl<'p, 'w, P: Ord> Iterator for SyllableIterator<'p, 'w, P> {
-    type Item = &'w [P];
+#[derive(Debug)]
+pub struct Syllable<'w> {
+    pub onset: &'w str,
+    pub vowel: &'w str,
+    pub coda: &'w str,
+}
+
+impl fmt::Display for Syllable<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str(self.onset)?;
+        f.write_str(self.vowel)?;
+        f.write_str(self.coda)?;
+        Ok(())
+    }
+}
+
+pub struct SyllableIterator<'p, 'w> {
+    phonology: &'p Phonology,
+    word: &'w str,
+    prev_syllable: SyllableIndices,
+}
+
+impl<'p, 'w> Iterator for SyllableIterator<'p, 'w> {
+    type Item = Syllable<'w>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.onset_start >= self.word.len() {
+        if self.prev_syllable.onset >= self.word.len() {
             None
         } else {
-            let (next_onset, next_coda) =
-                self.phonology.syllabize_first(self.word, self.coda_start);
-            let item = &self.word[self.onset_start..next_onset];
-            self.onset_start = next_onset;
-            self.coda_start = next_coda;
+            let next_syllable = self
+                .phonology
+                .syllabize_one(self.word, self.prev_syllable.coda);
+            let item = Syllable {
+                onset: &self.word[self.prev_syllable.onset..self.prev_syllable.vowel],
+                vowel: &self.word[self.prev_syllable.vowel..self.prev_syllable.coda],
+                coda: &self.word[self.prev_syllable.coda..next_syllable.onset],
+            };
+            self.prev_syllable = next_syllable;
             Some(item)
         }
     }
@@ -292,11 +396,11 @@ impl Theory {
 
 #[cfg(test)]
 mod tests {
-    use std::{fs::File, io::BufReader};
+    use std::{collections::BTreeSet, fs::File, io::BufReader, rc::Rc};
 
     use test_case::test_case;
 
-    use super::{Phonology, Theory};
+    use super::{OnsetCluster, Phoneme, Phonology, RawPhonology, Theory};
 
     #[test]
     fn load_theory() -> anyhow::Result<()> {
@@ -304,22 +408,44 @@ mod tests {
         Ok(())
     }
 
-    #[test_case("spun", &["spun"] ; "one syllable")]
-    #[test_case("epsom", &["ep", "som"] ; "vowel initial")]
-    #[test_case("malsprot", &["mal", "sprot"] ; "complex onset")]
+    fn phoneme_set<S>(it: impl IntoIterator<Item = S>) -> BTreeSet<Phoneme>
+    where
+        S: Into<Rc<str>>,
+    {
+        it.into_iter().map(|ph| Phoneme(ph.into())).collect()
+    }
+
+    #[test_case("sput", &["sput"] ; "one syllable")]
+    #[test_case("epsol", &["ep", "sol"] ; "vowel initial")]
+    #[test_case("talsprot", &["tal", "sprot"] ; "complex onset")]
+    #[test_case("tʃitʃrek", &["tʃit", "ʃrek"] ; "longer consonants")]
+    #[test_case("tejis", &["tej", "is"] ; "longer vowels")]
     fn syllabification(word: &str, expected_syllables: &[&str]) -> anyhow::Result<()> {
-        let phonology = Phonology {
-            onset_singles: "ptksrlmn".bytes().collect(),
-            onset_clusters: [(b'p', "rl"), (b't', "r"), (b'k', "rl"), (b's', "ptklmn")]
-                .into_iter()
-                .map(|(k, v)| (k, v.bytes().collect()))
-                .collect(),
-            vowels: "aeiou".bytes().collect(),
-        };
+        let phonology: Phonology = RawPhonology {
+            onset_singles: phoneme_set(["p", "t", "k", "tʃ", "s", "ʃ", "r", "l", "j"]),
+            onset_clusters: [
+                OnsetCluster {
+                    first: phoneme_set(["p", "k"]),
+                    second: phoneme_set(["r", "l"]),
+                },
+                OnsetCluster {
+                    first: phoneme_set(["t", "ʃ"]),
+                    second: phoneme_set(["r"]),
+                },
+                OnsetCluster {
+                    first: phoneme_set(["s"]),
+                    second: phoneme_set(["p", "t", "k", "l"]),
+                },
+            ]
+            .into_iter()
+            .collect(),
+            vowels: phoneme_set(["a", "e", "i", "o", "u", "ej"]),
+        }
+        .try_into()?;
         let actual_syllables = phonology
-            .syllabize_word(word.as_bytes())?
-            .map(std::str::from_utf8)
-            .collect::<Result<Vec<_>, _>>()?;
+            .syllabize_word(word)?
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>();
 
         assert_eq!(actual_syllables, expected_syllables);
         Ok(())
